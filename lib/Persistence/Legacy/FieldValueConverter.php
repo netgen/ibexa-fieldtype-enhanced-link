a79ddcb005af923dc178eb287190929b02bc558a
Petar Jakopec <petar.jakopec0@gmail.com>
2022-11-11 09:38:14 +0100

    NGSTACK-606 refactor FieldValueConverter

--- lib/Persistence/Legacy/FieldValueConverter.php
+++ lib/Persistence/Legacy/FieldValueConverter.php
@@ -9,6 +9,7 @@ use Ibexa\Contracts\Core\Persistence\Content\Type\FieldDefinition;
 use Ibexa\Core\Persistence\Legacy\Content\FieldValue\Converter;
 use Ibexa\Core\Persistence\Legacy\Content\StorageFieldDefinition;
 use Ibexa\Core\Persistence\Legacy\Content\StorageFieldValue;
+use Netgen\IbexaFieldTypeEnhancedLink\FieldType\Type;
 
 use function json_decode;
 use function json_encode;
@@ -26,9 +27,9 @@ class FieldValueConverter implements Converter
         if (empty($value->data['id'])) {
             $storageFieldValue->dataText = null;
         } else {
-            $storageFieldValue->dataText = json_encode($value->data, JSON_THROW_ON_ERROR);
+            $storageFieldValue->dataText = json_encode($value->data, JSON_PRETTY_PRINT | JSON_THROW_ON_ERROR);
         }
-        $storageFieldValue->sortKeyInt = (int) $value->sortKey;
+        $storageFieldValue->sortKeyString = (string) $value->sortKey;
     }
 
     /**
@@ -41,7 +42,7 @@ class FieldValueConverter implements Converter
         } else {
             $fieldValue->data = null;
         }
-        $fieldValue->sortKey = (int) $value->sortKeyInt;
+        $fieldValue->sortKey = (string) $value->sortKeyString;
     }
 
     /**
@@ -49,27 +50,7 @@ class FieldValueConverter implements Converter
      */
     public function toStorageFieldDefinition(FieldDefinition $fieldDef, StorageFieldDefinition $storageDef): void
     {
-        $jsonToSave = [];
-        $fieldSettings = $fieldDef->fieldTypeConstraints->fieldSettings;
-        $selectionMethod = isset($fieldSettings['selectionMethod']) ? (int) $fieldSettings['selectionMethod'] : 0;
-        $jsonToSave['selection_method'] = $selectionMethod;
-        $jsonToSave['root_default_location'] = (bool) $fieldSettings['rootDefaultLocation'];
-        if (!empty($fieldSettings['selectionRoot'])) {
-            $jsonToSave['current_object_placement'] = (string) $fieldSettings['selectionRoot'];
-        } else {
-            $jsonToSave['current_object_placement'] = 0;
-        }
-        $jsonToSave['allowed_content_types'] = $fieldSettings['selectionContentTypes'];
-        $jsonToSave['allowed_link_types'] = $fieldSettings['allowedLinkType'];
-        $jsonToSave['allowed_targets'] = $fieldSettings['allowedTargets'];
-
-        $storageDef->dataText5 = json_encode($jsonToSave, JSON_PRETTY_PRINT | JSON_THROW_ON_ERROR);
-
-        // BC: For Backwards Compatibility for legacy and in case of downgrades or data sharing
-        // Selection method, 0 = browse, 1 = dropdown
-        $storageDef->dataInt1 = $selectionMethod;
-        // Selection root, location ID, or 0 if empty
-        $storageDef->dataInt2 = (int) $fieldSettings['selectionRoot'];
+        $storageDef->dataText5 = json_encode($fieldDef->fieldTypeConstraints->fieldSettings, JSON_PRETTY_PRINT | JSON_THROW_ON_ERROR);
     }
 
     /**
@@ -79,37 +60,41 @@ class FieldValueConverter implements Converter
      */
     public function toFieldDefinition(StorageFieldDefinition $storageDef, FieldDefinition $fieldDef): void
     {
-        // default settings
-        // use dataInt1 and dataInt2 fields as default for backward compatibility
         $fieldDef->fieldTypeConstraints->fieldSettings = [
-            'selectionMethod' => $storageDef->dataInt1,
-            'selectionRoot' => $storageDef->dataInt2 === 0 ? '' : $storageDef->dataInt2,
+            'selectionMethod' => Type::SELECTION_BROWSE,
+            'selectionRoot' => null,
             'rootDefaultLocation' => true,
             'selectionContentTypes' => [],
-            'allowedTargets' => [],
-            'allowedLinkType' => [],
+            'allowedTargets' => [
+                Type::ALLOWED_TARGET_LINK,
+                Type::ALLOWED_TARGET_LINK_IN_NEW_TAB,
+                Type::ALLOWED_TARGET_IN_PLACE,
+                Type::ALLOWED_TARGET_MODAL,
+            ],
+            'allowedLinkType' => [
+                Type::ALLOWED_LINK_TYPE_EXTERNAL,
+                Type::ALLOWED_LINK_TYPE_INTERNAL,
+            ],
+            'enableQueryParameter' => false,
         ];
 
         if (empty($storageDef->dataText5)) {
             return;
         }
+        $decodedJson = json_decode($storageDef->dataText5, true, 512, JSON_THROW_ON_ERROR);
 
-        try {
-            $decodedJson = json_decode($storageDef->dataText5, true, 512, JSON_THROW_ON_ERROR);
-        } catch (\JsonException $e) {
-            return;
-        }
         $fieldSettings = &$fieldDef->fieldTypeConstraints->fieldSettings;
-        $fieldSettings['selectionMethod'] = $decodedJson['selection_method'];
-        $fieldSettings['selectionRoot'] = $decodedJson['current_object_placement'];
-        $fieldSettings['rootDefaultLocation'] = $decodedJson['root_default_location'];
-        $fieldSettings['selectionContentTypes'] = $decodedJson['allowed_content_types'];
-        $fieldSettings['allowedTargets'] = $decodedJson['allowed_targets'];
-        $fieldSettings['allowedLinkType'] = $decodedJson['allowed_link_types'];
+        $fieldSettings['selectionMethod'] = $decodedJson['selectionMethod'];
+        $fieldSettings['selectionRoot'] = $decodedJson['selectionRoot'];
+        $fieldSettings['rootDefaultLocation'] = $decodedJson['rootDefaultLocation'];
+        $fieldSettings['selectionContentTypes'] = $decodedJson['selectionContentTypes'];
+        $fieldSettings['allowedTargets'] = $decodedJson['allowedTargets'];
+        $fieldSettings['allowedLinkType'] = $decodedJson['allowedLinkType'];
+        $fieldSettings['enableQueryParameter'] = $decodedJson['enableQueryParameter'];
     }
 
     public function getIndexColumn(): string
     {
-        return 'sort_key_int';
+        return 'sort_key_string';
     }
 }
