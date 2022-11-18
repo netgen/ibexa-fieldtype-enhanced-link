c00c1f1afb9e5eaf93a9f3afb8559f1d8862982c
Petar Jakopec <petar.jakopec0@gmail.com>
2022-11-18 14:10:57 +0100

    NGSTACK-606 implement field value edit form

--- lib/Form/Field/FieldValueType.php
+++ lib/Form/Field/FieldValueType.php
@@ -7,10 +7,16 @@ namespace Netgen\IbexaFieldTypeEnhancedLink\Form\Field;
 use Ibexa\Contracts\Core\Repository\ContentService;
 use Ibexa\Contracts\Core\Repository\ContentTypeService;
 use Ibexa\Contracts\Core\Repository\Exceptions\UnauthorizedException;
+use Ibexa\Contracts\Core\Repository\FieldTypeService;
 use Ibexa\Contracts\Core\Repository\Values\Content\Location;
+use JMS\TranslationBundle\Annotation\Desc;
+use Netgen\IbexaFieldTypeEnhancedLink\FieldType\Type;
 use Netgen\IbexaFieldTypeEnhancedLink\FieldType\Value;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
@@ -20,15 +26,21 @@ class FieldValueType extends AbstractType
 {
     private ContentService $contentService;
     private ContentTypeService $contentTypeService;
+    private FieldTypeService $fieldTypeService;
 
     /**
      * @param \Ibexa\Contracts\Core\Repository\ContentService $contentService
      * @param \Ibexa\Contracts\Core\Repository\ContentTypeService $contentTypeService
      */
-    public function __construct(ContentService $contentService, ContentTypeService $contentTypeService)
+    public function __construct(
+        ContentService $contentService,
+        ContentTypeService $contentTypeService,
+        FieldTypeService $fieldTypeService
+    )
     {
         $this->contentService = $contentService;
         $this->contentTypeService = $contentTypeService;
+        $this->fieldTypeService = $fieldTypeService;
     }
 
     public function getName(): string
@@ -41,14 +53,92 @@ class FieldValueType extends AbstractType
         return 'ibexa_fieldtype_ngenhancedlink';
     }
 
-    public function getParent(): string
-    {
-        return IntegerType::class;
-    }
-
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->addModelTransformer(new FieldValueTransformer());
+        $builder
+            ->add(
+                'link_type',
+                ChoiceType::class,
+                [
+                    'choices' => [
+                        'field_edit.ngenhancedlink.link_type.' . Type::LINK_TYPE_INTERNAL => Type::LINK_TYPE_INTERNAL,
+                        'field_edit.ngenhancedlink.link_type.' . Type::LINK_TYPE_EXTERNAL => Type::LINK_TYPE_EXTERNAL,
+                    ],
+                    'label' => /* @Desc("Text") */ 'field_edit.ngenhancedlink.link_type',
+                    'required' => true,
+                    'multiple' => false,
+                    'expanded' => true,
+                ],
+            )
+            ->add(
+                'id',
+                IntegerType::class,
+                [
+                    'label' => /* @Desc("Text") */ 'field_edit.ngenhancedlink.internal_id',
+                    'required' => false,
+                ],
+            )
+            ->add(
+                'suffix',
+                TextType::class,
+                [
+                    'label' => /* @Desc("Text") */ 'field_edit.ngenhancedlink.suffix',
+                    'required' => false,
+                ],
+            )
+            ->add(
+                'label_internal',
+                TextType::class,
+                [
+                    'label' => /* @Desc("Text") */ 'field_edit.ngenhancedlink.label',
+                    'required' => false,
+                ],
+            )
+            ->add(
+                'target_internal',
+                ChoiceType::class,
+                [
+                    'choices' => [
+                        'field_edit.ngenhancedlink.target.' . Type::TARGET_LINK => Type::TARGET_LINK,
+                        'field_edit.ngenhancedlink.target.' . Type::TARGET_LINK_IN_NEW_TAB => Type::TARGET_LINK_IN_NEW_TAB,
+                        'field_edit.ngenhancedlink.target.' . Type::TARGET_EMBED => Type::TARGET_EMBED,
+                        'field_edit.ngenhancedlink.target.' . Type::TARGET_MODAL => Type::TARGET_MODAL,
+                    ],
+                    'label' => /* @Desc("Text") */ 'field_edit.ngenhancedlink.target',
+                    'required' => true,
+                ],
+            )
+            ->add(
+                'url',
+                UrlType::class,
+                [
+                    'label' => /* @Desc("URL") */ 'field_edit.ngenhancedlink.url',
+                    'required' => $options['required'],
+                ],
+            )
+            ->add(
+                'label_external',
+                TextType::class,
+                [
+                    'label' => /* @Desc("Text") */ 'field_edit.ngenhancedlink.label',
+                    'required' => false,
+                ],
+            )
+            ->add(
+                'target_external',
+                ChoiceType::class,
+                [
+                    'choices' => [
+                        'field_edit.ngenhancedlink.target.' . Type::TARGET_LINK => Type::TARGET_LINK,
+                        'field_edit.ngenhancedlink.target.' . Type::TARGET_LINK_IN_NEW_TAB => Type::TARGET_LINK_IN_NEW_TAB,
+                        'field_edit.ngenhancedlink.target.' . Type::TARGET_EMBED => Type::TARGET_EMBED,
+                        'field_edit.ngenhancedlink.target.' . Type::TARGET_MODAL => Type::TARGET_MODAL,
+                    ],
+                    'label' => /* @Desc("Text") */ 'field_edit.ngenhancedlink.target',
+                    'required' => true,
+                ],
+            )
+            ->addModelTransformer(new FieldValueTransformer($this->fieldTypeService->getFieldType('ngenhancedlink')));
     }
 
     /**
@@ -63,9 +153,10 @@ class FieldValueType extends AbstractType
         /** @var \Netgen\IbexaFieldTypeEnhancedLink\FieldType\Value $data */
         $data = $form->getData();
 
-        if (!$data instanceof Value || null === $data->reference) {
+        if (!$data instanceof Value || null === $data->reference || $data->isExternal()) {
             return;
         }
+
         $contentId = $data->reference;
         $contentInfo = null;
         $contentType = null;
@@ -86,9 +177,10 @@ class FieldValueType extends AbstractType
         ];
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
+            'translation_domain' => 'field_edit',
             'attr' => [
                 'min' => 1,
                 'step' => 1,
@@ -97,7 +189,6 @@ class FieldValueType extends AbstractType
             'root_default_location' => null,
             'location' => null,
         ]);
-
         $resolver->setAllowedTypes('default_location', ['null', Location::class]);
         $resolver->setAllowedTypes('root_default_location', ['null', 'bool']);
         $resolver->setAllowedTypes('location', ['null', Location::class]);
