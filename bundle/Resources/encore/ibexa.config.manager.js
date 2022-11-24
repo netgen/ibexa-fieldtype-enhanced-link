63a41bcdac379644968804cb3ad8154a1e091f34
amalija-ramljak <amalija@netgen.io>
2022-11-24 13:17:35 +0100

    NGSTACK-606 refactor structure

--- bundle/Resources/encore/ibexa.config.manager.js
+++ bundle/Resources/encore/ibexa.config.manager.js
@@ -1,27 +1,27 @@
 const path = require('path');
 
 module.exports = (ibexaConfig, ibexaConfigManager) => {
+    /** Content type editing */
     ibexaConfigManager.add({
         ibexaConfig,
         entryName: 'ibexa-admin-ui-content-type-edit-js',
-        newItems: [path.resolve(__dirname, '../public/js/scripts/admin.contenttype.ngenhancedlink.default.location.js')],
+        newItems: [path.resolve(__dirname, '../public/admin/field_definition.js')],
     });
-
-    ibexaConfigManager.add({
-        ibexaConfig,
-        entryName: 'ibexa-admin-ui-content-edit-parts-js',
-        newItems: [path.resolve(__dirname, '../public/js/scripts/fieldType/ngenhancedlink.js')],
-    });
-
-    ibexaConfigManager.add({
-        ibexaConfig,
-        entryName: 'ibexa-admin-ui-content-edit-parts-css',
-        newItems: [path.resolve(__dirname, '../public/scss/fieldType/edit/_ngenhancedlink.scss')],
-    });
-    
     ibexaConfigManager.add({
         ibexaConfig,
         entryName: 'ibexa-admin-ui-layout-css',
-        newItems: [path.resolve(__dirname, '../public/scss/fieldType/definition/_ngenhancedlink.scss')],
+        newItems: [path.resolve(__dirname, '../public/field_definition.scss')],
+    });
+
+    /** Content editing */
+    ibexaConfigManager.add({
+        ibexaConfig,
+        entryName: 'ibexa-admin-ui-content-edit-parts-js',
+        newItems: [path.resolve(__dirname, '../public/admin/field.js')],
+    });
+    ibexaConfigManager.add({
+        ibexaConfig,
+        entryName: 'ibexa-admin-ui-content-edit-parts-css',
+        newItems: [path.resolve(__dirname, '../public/admin/field.scss')],
     });
 };
